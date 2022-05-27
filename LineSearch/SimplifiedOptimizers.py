b6cf167ba6db50fae708c6445e04cf3f0885b19b
infamoussoap <james.chok1000+github@gmail.com>
2022-05-27 18:22:31 +1000

    changing how to find the max learning rate

--- LineSearch/SimplifiedOptimizers.py
+++ LineSearch/SimplifiedOptimizers.py
@@ -17,12 +17,8 @@ def squared_optimizer(points, y, kkt_tol=1e-3, max_iter=1000, tol=1e-8):
         dw_dt = w * (grad - w @ grad)
         cauchy_learning_rate = dw_dt @ grad / np.sum((dw_dt @ points) ** 2)
 
-        mask = dw_dt > tol
-        if np.any(mask):  # Check non-empty
-            max_learning_rate = np.min(w[mask] / dw_dt[mask])
-            learning_rate = min(cauchy_learning_rate, max_learning_rate)
-        else:
-            learning_rate = cauchy_learning_rate
+        max_learning_rate = 1 / (np.max(grad) - w @ grad)
+        learning_rate = min(cauchy_learning_rate, max_learning_rate)
 
         w = w - learning_rate * dw_dt
         w = w / np.sum(w)
