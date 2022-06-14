7f3bbdaecb6e6cfa52b66f58e376e599552234dd
infamoussoap <james.chok1000+github@gmail.com>
2022-06-14 14:06:46 +1000

    squared optimizer updated to reset once there too many zeros

--- ConvexHullOptimizers/Optimizers.py
+++ ConvexHullOptimizers/Optimizers.py
@@ -7,53 +7,6 @@ from .BisectionMethod import BisectionMethod
 from .utils import project_onto_standard_simplex, verbose_callback
 
 
-def squared_optimizer(points, y, kkt_tol=1e-3, max_iter=-1, verbose=False, w=None):
-    if w is None:
-        w = np.ones(len(points)) / len(points)
-    else:
-        assert abs(np.sum(w) - 1) < 1e-10, "w must sum to 1"
-        assert len(w) == len(points), "Length of w must be the same length as the hull"
-
-    status = 'Failed'
-
-    count = 0
-    while count < max_iter or max_iter < 0:
-        grad = (w @ points - y) @ points.T
-        if validate_kkt_conditions(w, grad, kkt_tol):
-            status = 'KKT'
-            break
-
-        dw_dt = w * (grad - w @ grad)
-        cauchy_learning_rate = dw_dt @ grad / np.sum((dw_dt @ points) ** 2)
-
-        masked_grad = grad * non_active_set
-        i = np.argmax(masked_grad)
-
-        max_learning_rate = 1 / (masked_grad[i] - w @ grad)
-        learning_rate = min(cauchy_learning_rate, max_learning_rate)
-
-        w = w - learning_rate * dw_dt
-
-        if learning_rate == max_learning_rate:
-            non_active_set[i] = 0
-            w[i] = 0
-
-        w = w / np.sum(w)
-
-        if verbose:
-            verbose_callback(count, max_iter, w, points, y)
-
-        count += 1
-
-    distance = np.sum((w @ points - y) ** 2)
-
-    if verbose:
-        sys.stdout.write('\n')
-        sys.stdout.flush()
-
-    return status, distance, count + 1, w
-
-
 def egd_optimizer(points, y, kkt_tol=1e-3, max_iter=-1, verbose=False, w=None):
     if w is None:
         w = np.ones(len(points)) / len(points)
