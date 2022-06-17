aa37a2778167bd4c63f8214db6531178f4b0211e
infamoussoap <james.chok1000+github@gmail.com>
2022-06-17 11:56:01 +1000

    original implementation was wrong, now fixed

--- ConvexHullOptimizers/PGDOptimizer.py
+++ ConvexHullOptimizers/PGDOptimizer.py
@@ -13,25 +13,19 @@ def pgd_optimizer(points, y, kkt_tol=1e-3, max_iter=-1, verbose=False, w=None, t
         assert abs(np.sum(w) - 1) < 1e-10, "w must sum to 1"
         assert len(w) == len(points), "Length of w must be the same length as the hull"
 
-    indices = np.arange(len(w))
-    original_length = len(w)
-
     status = 'Failed'
 
     count = 0
     while count < max_iter or max_iter < 0:
-        mask = w > tol
-        w = w[mask]
-        points = points[mask]
-        indices = indices[mask]
-
         grad = (w @ points - y) @ points.T
         if validate_kkt_conditions(w, grad, kkt_tol):
             status = 'KKT'
             break
 
         normalized_grad = (grad - np.mean(grad)) / len(grad)
-        max_learning_rate = 1 / np.max(normalized_grad / w)
+
+        mask = w > tol
+        max_learning_rate = 1 / np.max(normalized_grad[mask] / w[mask])
 
         cauchy_learning_rate = normalized_grad @ grad / np.sum((normalized_grad @ points) ** 2)
 
@@ -50,9 +44,5 @@ def pgd_optimizer(points, y, kkt_tol=1e-3, max_iter=-1, verbose=False, w=None, t
     if verbose:
         sys.stdout.write('\n')
         sys.stdout.flush()
-
-    w_copy = w.copy()
-    w = np.zeros(original_length)
-    w[indices] = w_copy
 
     return status, distance, count + 1, w
