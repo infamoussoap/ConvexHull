334e9381fd9644bba89f4471b374397f70bc64fd
James Chok <james.chok1000+github@gmail.com>
2022-09-10 11:48:57 -0400

    removing ConvexHullOptimizers, will now only use SimplifiedOptimizers

--- ConvexHullOptimizers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .SquaredOptimizer import squared_optimizer
-from .EGDOptimizer import egd_optimizer
-from .PGDOptimizer import pgd_optimizer
-from .FrankWolfeOptimizer import frank_wolfe_optimizer
