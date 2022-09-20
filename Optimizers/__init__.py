b48dc7e09fedbaf81fe28e8f4129fd621ad1b2a3
James Chok <james.chok1000+github@gmail.com>
2022-09-20 11:27:27 +0100

    setting aliasis for the optimizers

--- Optimizers/__init__.py
+++ Optimizers/__init__.py
@@ -1,4 +1,10 @@
-from .CauchySimplex import cauchy_simplex_optimizer
 from .EGD import egd_optimizer
+
 from .PairwiseFrankWolfe import pairwise_frank_wolfe_optimizer
+from .PairwiseFrankWolfe import pairwise_frank_wolfe_optimizer as pfw_optimizer
+
 from .FrankWolfe import frank_wolfe_optimizer
+from .FrankWolfe import frank_wolfe_optimizer as fw_optimizer
+
+from .CauchySimplex import cauchy_simplex_optimizer
+from .CauchySimplex import cauchy_simplex_optimizer as cs_optimizer
