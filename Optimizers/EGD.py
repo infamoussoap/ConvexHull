89f8cddc3130eb3c08a04ff63413d1213baf119b
James Chok <james.chok1000+github@gmail.com>
2022-09-27 13:38:51 +0100

    limit of t_max changed, and less than e is checked only when it is finished

--- Optimizers/EGD.py
+++ Optimizers/EGD.py
@@ -20,15 +20,12 @@ def egd_optimizer(X, y, max_iter=-1, verbose=False, w=None, tol=1e-6, e=1e-10,
     while count < max_iter or max_iter < 0:
         grad = (w @ X - y) @ X.T
 
-        t_max = min(2 * (count + 1), 1000)
+        t_max = min(2 * (count + 1), abs(100 / np.max(grad)))
         learning_rate = search_method.search(w, y, t_max, grad, search_type='classical')
 
         z = -learning_rate * grad
         x = w * np.exp(z - np.max(z))  # For numerical stability
         w = x / np.sum(x)
-
-        w[w < e] = 0
-        w = w / np.sum(w)
 
         count += 1
 
@@ -41,6 +38,9 @@ def egd_optimizer(X, y, max_iter=-1, verbose=False, w=None, tol=1e-6, e=1e-10,
     if verbose:
         sys.stdout.write('\n')
         sys.stdout.flush()
+
+    w[w < e] = 0
+    w = w / np.sum(w)
 
     distance = np.sum((w @ X - y) ** 2) / 2
     return distance, count + 1, w
