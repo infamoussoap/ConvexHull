98a5a25b290bb2c73a4e0fbcc021dfc8e70ab8b6
James Chok <james.chok1000+github@gmail.com>
2022-10-26 10:27:17 +0100

    comining cs and pfw

--- Optimizers/__init__.py
+++ Optimizers/__init__.py
@@ -14,3 +14,6 @@ from .CauchySimplexV2 import cauchy_simplex_v2_optimizer as cs_v2_optimizer
 
 from .CauchySimplexV3 import cauchy_simplex_v3_optimizer
 from .CauchySimplexV3 import cauchy_simplex_v3_optimizer as cs_v3_optimizer
+
+from .CauchySimplexV4 import cauchy_simplex_v4_optimizer
+from .CauchySimplexV4 import cauchy_simplex_v4_optimizer as cs_v4_optimizer
