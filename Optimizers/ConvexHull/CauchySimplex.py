8c4cde444a0e4978e9414756a670107c1ff90b2b
James Chok <james.chok1000+github@gmail.com>
2023-01-22 17:30:32 +0000

    chainging e to tol

--- Optimizers/ConvexHull/CauchySimplex.py
+++ Optimizers/ConvexHull/CauchySimplex.py
@@ -6,13 +6,13 @@ from Optimizers.utils import clip
 
 
 class CauchySimplex(ConvexHull, ArmijoSearch, Optimizer):
-    def __init__(self, X, y, e=1e-10):
+    def __init__(self, X, y, tol=1e-10):
         ConvexHull.__init__(self, X, y)
-        self.e = e
+        self.tol = tol
 
     def update(self, x, d, step_size):
         z = x - step_size * d
-        z[x < self.e] = 0
+        z[x < self.tol] = 0
 
         return z / np.sum(z)
 
@@ -20,13 +20,13 @@ class CauchySimplex(ConvexHull, ArmijoSearch, Optimizer):
         grad = self.f(x, grad=True)
         d = x * (grad - grad @ x)
 
-        max_step_size = self.max_step_size(x, grad, e=self.e) if step_size is None else step_size
+        max_step_size = self.max_step_size(x, grad, tol=self.tol) if step_size is None else step_size
         cauchy_step_size = d @ grad / np.sum((d @ self.X) ** 2)
 
         step_size = clip(cauchy_step_size, 0, max_step_size)
         return self.update(x, d, step_size)
 
     @staticmethod
-    def max_step_size(x, grad, e=1e-10):
-        support = x > e
+    def max_step_size(x, grad, tol=1e-10):
+        support = x > tol
         return 1 / (np.max(grad[support]) - x @ grad)
