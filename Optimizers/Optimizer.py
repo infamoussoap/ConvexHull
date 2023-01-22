43cb115a0271a6e4f12e60982c4a09b62046842d
James Chok <james.chok1000+github@gmail.com>
2023-01-22 14:48:53 +0000

    forgot to add Optimizer abstract class

---
+++ Optimizers/Optimizer.py
@@ -0,0 +1,12 @@
+from abc import ABC
+from abc import abstractmethod
+
+
+class Optimizer(ABC):
+    @abstractmethod
+    def update(self, x, d, step_size):
+        pass
+
+    @abstractmethod
+    def search(x, step_size=1, c1=1e-4, c2=0.5, max_iter=100):
+        pass
--- Optimizers/__init__.py
+++ Optimizers/__init__.py
@@ -1,10 +1,2 @@
-from .EGD import egd_optimizer
-
-from .PairwiseFrankWolfe import pairwise_frank_wolfe_optimizer
-from .PairwiseFrankWolfe import pairwise_frank_wolfe_optimizer as pfw_optimizer
-
-from .FrankWolfe import frank_wolfe_optimizer
-from .FrankWolfe import frank_wolfe_optimizer as fw_optimizer
-
-from .CauchySimplex import cauchy_simplex_optimizer
-from .CauchySimplex import cauchy_simplex_optimizer as cs_optimizer
+from .ArmijoSearch import ArmijoSearch
+from .Optimizer import Optimizer
