faa37714680d17880359f8315d037267f80aaa56
James Chok <james.chok1000+github@gmail.com>
2023-01-31 16:35:21 +0000

    adding validate_stopping_condition to init file

--- Optimizers/ConvexHull/__init__.py
+++ Optimizers/ConvexHull/__init__.py
@@ -1,3 +1,4 @@
 from .CauchySimplex import CauchySimplex
 from .EGD import EGD
 from .PairwiseFrankWolfe import PairwiseFrankWolfe
+from .KKTConditions.StoppingCondition import validate_stopping_conditions
