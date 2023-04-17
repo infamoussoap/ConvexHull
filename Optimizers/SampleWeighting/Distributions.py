70d2b2d36f64ed395c59af7a529d928739189a86
James Chok <james.chok1000+github@gmail.com>
2023-04-17 14:04:25 +0100

    adding truncated gaussian

--- Optimizers/SampleWeighting/Distributions.py
+++ Optimizers/SampleWeighting/Distributions.py
@@ -1,10 +1,10 @@
 import numpy as np
 from scipy.special import erf
 
+C = np.sqrt(2 * np.pi)
+
 
 class UnitNormal:
-    C = np.sqrt(2 * np.pi)
-
     def __call__(self, x, mu, std, grad=False):
         """ Returns the pdf evaluated at the x-points
 
@@ -17,18 +17,18 @@ class UnitNormal:
         scaled_x = (x[:, None] - mu[None, :]) / std
 
         if grad:
-            return -(scaled_x / self.C) * np.exp(-0.5 * (scaled_x ** 2))
+            return -(scaled_x / C) * np.exp(-0.5 * (scaled_x ** 2))
 
-        return np.exp(-0.5 * (scaled_x ** 2)) / self.C
+        return np.exp(-0.5 * (scaled_x ** 2)) / C
 
 
 class TruncatedUnitNormal:
     unit_normal = UnitNormal()
-    
+
     def __init__(self, a=0, b=1):
         self.a = a
         self.b = b
-        
+
         self.c = self._Phi(self.b) - self._Phi(self.a)
 
     def __call__(self, x, mu, std, grad=False):
@@ -52,7 +52,6 @@ class TruncatedUnitNormal:
 
 
 class Gaussian:
-    C = np.sqrt(2 * np.pi)
 
     def __init__(self, mu, std):
         self.mu = mu
@@ -60,6 +59,30 @@ class Gaussian:
 
     def __call__(self, x, grad=False):
         if grad:
-            return ((self.mu - x) / (self.C * self.std ** 3)) * np.exp(-0.5 * ((x - self.mu) / self.std) ** 2)
+            return ((self.mu - x) / (C * self.std ** 3)) * np.exp(-0.5 * ((x - self.mu) / self.std) ** 2)
 
-        return np.exp(-0.5 * ((x - self.mu) / self.std) ** 2) / (self.std * self.C)
+        return np.exp(-0.5 * ((x - self.mu) / self.std) ** 2) / (self.std * C)
+
+
+class TruncatedGaussian:
+    def __init__(self, mu, std, a=0, b=1):
+        self.gaussian = Gaussian(mu, std)
+
+        self.a = a
+        self.b = b
+
+        self.c = (self._Phi((self.b - mu) / std) - self._Phi((self.a - mu) / std))
+
+    def __call__(self, x, grad=False):
+        """ Returns the pdf evaluated at the x-points
+
+            Parameters
+            ----------
+            x : (n,) np.ndarray
+        """
+        mask = (self.a <= x) * (x <= self.b)
+        return self.gaussian(x, grad=grad) * mask / self.c
+
+    @staticmethod
+    def _Phi(x):
+        return (1 + erf(x / np.sqrt(2))) / 2
