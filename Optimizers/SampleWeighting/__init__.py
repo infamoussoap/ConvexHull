33ff95a95be5c2cca56f399eee8bd6b4d2429f7c
James Chok <james.chok1000+github@gmail.com>
2023-05-09 09:18:41 +0100

    removing ECS

--- Optimizers/SampleWeighting/__init__.py
+++ Optimizers/SampleWeighting/__init__.py
@@ -1,4 +1,3 @@
 from .CauchySimplex import CauchySimplex
 from .EGD import EGD
 from .PairwiseFrankWolfe import PairwiseFrankWolfe
-from .ExponentiatedCauchySimplex import ExponentiatedCauchySimplex
